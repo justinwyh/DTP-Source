2a070a57748d207fa6abf2aa6aed38c6bf313a33
Justin <37975524+justinwyh@users.noreply.github.com>
2020-04-05 22:42:21 +0800

    Add AppConfig.cs in TrackingWPF;

---
+++ DTP/TrackingWPF/Utils/APPConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.Storage;
+
+namespace TrackingWPF.Utils
+{
+    class APPConfig
+    {
+        ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+
+        public readonly static APPConfig instance = new APPConfig();
+
+        public string getConfigProperties(String PropertyName)
+        {
+            return localSettings.Values[PropertyName] as String;
+        }
+
+        public void setConfigProperties(string PropertyName, string value)
+        {
+            localSettings.Values[PropertyName] = value;
+        }
+    }
+}
